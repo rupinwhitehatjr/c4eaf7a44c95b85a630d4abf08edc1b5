206f1da353d367ef7ba9f2bc18ad98fd1b8472bf
rupinwhitehatjr <61224617+rupinwhitehatjr@users.noreply.github.com>
2020-08-05 15:17:08 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function FillDetails(){ 
+ 
+ var first = document.getElementById("input1").value;
+   document.getElementById("blank1").innerHTML=first.toUpperCase();
+  
+   var second = document.getElementById("input2").value;
+   document.getElementById("blank2").innerHTML=second.toUpperCase();
+  
+  
+  var third = document.getElementById("input3").value;
+   document.getElementById("blank3").innerHTML=third.toUpperCase();
+  
+  var fourth = document.getElementById("input4").value;
+   document.getElementById("blank4").innerHTML=fourth.toUpperCase();
+  
+  var fifth = document.getElementById("input5").value;
+   document.getElementById("blank5").innerHTML=fifth.toUpperCase();
+}
